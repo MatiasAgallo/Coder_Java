afafeec70d7b3c9084815a6da910d663b7a218ff
Matias Gallo <matiasgallo2407@gmail.com>
2024-12-10 19:56:04 -0300

    Update

---
+++ Clase-01/src/main/java/com/coderhouse/Clase01.java
@@ -0,0 +1,20 @@
+package com.coderhouse;
+
+public class Clase01 {
+
+	public static void main(String[] args) {
+		
+		//System.out.println("Hola Mundo");
+		
+		byte variableA = 100;
+		double variablesConDecimales = 2.1456d;
+		char miPrimerChar = 'H';
+		String miPrimerString = "Como estan?";
+		boolean booleano = true;
+		long numeroLargo = 20394095L;
+		String saludoInicial = "Hola coders";
+		System.out.println(saludoInicial + ' ' + miPrimerString);
+		
+	}
+
+}
