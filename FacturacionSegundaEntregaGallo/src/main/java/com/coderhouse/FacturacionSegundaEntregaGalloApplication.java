072547edc943d78b13a0f45e274fd6a8436493db
Matias Gallo <matiasgallo2407@gmail.com>
2025-01-15 05:07:48 -0300

    Update

---
+++ FacturacionSegundaEntregaGallo/src/main/java/com/coderhouse/FacturacionSegundaEntregaGalloApplication.java
@@ -0,0 +1,13 @@
+package com.coderhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionSegundaEntregaGalloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionSegundaEntregaGalloApplication.class, args);
+	}
+
+}
